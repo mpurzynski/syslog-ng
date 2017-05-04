fbd07b7b8a3eb4de2a34eddec9c394ca19f06acf
Gabor Nagy <gabor.nagy@balabit.com>
2017-05-04 11:26:22 +0200

    snmptrapd-parser: adding new module
    Signed-off-by: Gabor Nagy <gabor.nagy@balabit.com>

---
+++ modules/snmptrapd-parser/snmptrapd-parser-parser.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Gabor Nagy
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "driver.h"
+#include "cfg-parser.h"
+#include "snmptrapd-parser-grammar.h"
+
+extern int snmptrapd_parser_debug;
+
+int snmptrapd_parser_parse(CfgLexer *lexer, LogDriver **instance, gpointer arg);
+
+static CfgLexerKeyword snmptrapd_parser_keywords[] =
+{
+  { NULL }
+};
+
+CfgParser snmptrapd_parser_parser =
+{
+#if ENABLE_DEBUG
+  .debug_flag = &snmptrapd_parser_debug,
+#endif
+  .name = "snmptrapd-parser",
+  .keywords = snmptrapd_parser_keywords,
+  .parse = (gint (*)(CfgLexer *, gpointer *, gpointer)) snmptrapd_parser_parse,
+  .cleanup = (void (*)(gpointer)) log_pipe_unref,
+};
+
+CFG_PARSER_IMPLEMENT_LEXER_BINDING(snmptrapd_parser_, LogDriver **)
+
---
+++ modules/snmptrapd-parser/snmptrapd-parser.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2017 Balabit
+ * Copyright (c) 2017 Gabor Nagy <gabor.nagy@balabit.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ */
+#include "snmptrapd-parser.h"
+
+#include <string.h>
+#include <ctype.h>
+
+typedef struct _SnmpTrapdParser
+{
+  LogParser super;
+  gchar *prefix;
+  LogTemplate *message_template;
+  LogTemplateOptions message_template_options;
+}SnmpTrapdParser;
+
+void 
+snmptrapd_parser_set_prefix(LogParser *s, const gchar *prefix)
+{
+  SnmpTrapdParser *self = (SnmpTrapdParser *) s;
+
+  g_free(self->prefix);
+  self->prefix = g_strdup(prefix);
+}
+
+static gboolean
+snmptrapd_parser_process(LogParser *s, LogMessage **pmsg,
+                const LogPathOptions *path_options,
+                const gchar *input, gsize input_len)
+{
+  return TRUE;
+}
+
+static void
+snmptrapd_parser_free(LogPipe *s)
+{
+  SnmpTrapdParser *self = (SnmpTrapdParser *) s;
+
+  log_parser_free_method(s);
+}
+
+LogParser*
+snmptrapd_parser_new(GlobalConfig *cfg)
+{
+  SnmpTrapdParser *self = g_new0(SnmpTrapdParser, 1);
+
+  log_parser_init_instance(&self->super, cfg);
+  //self->super.super.init = snmptrapd_parser_init;
+  //self->super.super.deinit = snmptrapd_parser_deinit;
+  self->super.super.free_fn = snmptrapd_parser_free;
+  //self->super.super.clone = snmptrapd_parser_clone;
+  self->super.process = snmptrapd_parser_process;
+
+  return &self->super;
+}
