a911f29380f22e276d67a1ded82f4e0fa1cf6964
László Várady <laszlo.varady@balabit.com>
2017-05-10 17:41:17 +0200

    !WIP snmptrapd-parser: thread safety trick + date parser

--- modules/snmptrapd-parser/snmptrapd-parser.c
+++ modules/snmptrapd-parser/snmptrapd-parser.c
@@ -22,6 +22,8 @@
 
 #include "snmptrapd-parser.h"
 #include "varbindlist-scanner.h"
+#include "str-format.h"
+#include "timeutils.h"
 
 #include <string.h>
 #include <ctype.h>
@@ -29,10 +31,13 @@
 typedef struct _SnmpTrapdParser
 {
   LogParser super;
+  // Use GString instead of prefix+prefixlen
   gchar *prefix;
+  gsize prefix_len;
   LogTemplate *message_template;
   LogTemplateOptions template_options;
 
+  GString *formatted_key;
   VarBindListScanner *varbindlist_scanner;
 } SnmpTrapdParser;
 
@@ -42,7 +47,17 @@ snmptrapd_parser_set_prefix(LogParser *s, const gchar *prefix)
   SnmpTrapdParser *self = (SnmpTrapdParser *) s;
 
   g_free(self->prefix);
-  self->prefix = g_strdup(prefix);
+
+  if (!prefix)
+    {
+      self->prefix = NULL;
+      self->prefix_len = 0;
+    }
+  else
+    {
+      self->prefix = g_strdup(prefix);
+      self->prefix_len = strlen(prefix);
+    }
 
   msg_trace("snmptrapd_parser_set_prefix", evt_tag_str("prefix", self->prefix));
 }
@@ -66,6 +81,123 @@ snmptrapd_parser_get_template_options(LogParser *s)
   return &self->template_options;
 }
 
+static const gchar *
+_get_formatted_key(SnmpTrapdParser *self, const gchar *key)
+{
+  if (!self->prefix)
+    return key;
+  else if (self->formatted_key->len > 0)
+    g_string_truncate(self->formatted_key, self->prefix_len);
+  else
+    g_string_assign(self->formatted_key, self->prefix);
+  g_string_append(self->formatted_key, key);
+  return self->formatted_key->str;
+}
+
+// DUPLICATED
+static inline void
+_skip_whitespaces(const gchar **input, gsize *input_len)
+{
+  const gchar *current_char = *input;
+
+  while (*input_len > 0 && g_ascii_isspace(*current_char))
+    {
+      ++current_char;
+      --(*input_len);
+    }
+
+  *input = current_char;
+}
+
+/*static
+_try_parse_v1_info()
+{
+  _try_v1_scan_enterprise_string();
+  \n
+    _try_v1_scan_trap_description_and_subtype();
+    _try_v1_scan_uptime();
+    \n
+}*/
+
+static gboolean
+_parse_timestamp(LogMessage *msg, const gchar **input, gsize *input_len)
+{
+  GTimeVal now;
+  cached_g_current_time(&now);
+  time_t now_tv_sec = (time_t) now.tv_sec;
+
+  LogStamp *stamp = &msg->timestamps[LM_TS_STAMP];
+  stamp->tv_usec = 0;
+  stamp->zone_offset = -1;
+
+  /* NOTE: we initialize various unportable fields in tm using a
+   * localtime call, as the value of tm_gmtoff does matter but it does
+   * not exist on all platforms and 0 initializing it causes trouble on
+   * time-zone barriers */
+
+  struct tm tm;
+  cached_localtime(&now_tv_sec, &tm);
+  if (!scan_std_timestamp(input, (gint *)input_len, &tm))
+    return FALSE;
+
+  stamp->tv_sec = cached_mktime(&tm);
+  stamp->zone_offset = get_local_timezone_ofs(stamp->tv_sec);
+
+  return TRUE;
+}
+
+static gboolean
+_parse_header(SnmpTrapdParser *self, LogMessage *msg, const gchar **input, gsize *input_len)
+{
+  _skip_whitespaces(input, input_len);
+
+  if (!_parse_timestamp(msg, input, input_len))
+    return FALSE;
+
+  _skip_whitespaces(input, input_len);
+
+  /*
+  _parse_hostname();
+  _parse_transport_info();
+  :
+  _try_parse_v1_info();*/
+
+
+
+  const gchar *next_line = strchr(*input, '\n');
+  if (!next_line)
+    return FALSE;
+
+  *input_len -= (next_line + 1) - *input;
+  *input = next_line + 1;
+
+  return TRUE;
+}
+
+static gboolean
+_parse_varbindlist(SnmpTrapdParser *self, LogMessage *msg, const gchar **input, gsize *input_len)
+{
+  const gchar *key, *value;
+
+  varbindlist_scanner_input(self->varbindlist_scanner, *input);
+  while (varbindlist_scanner_scan_next(self->varbindlist_scanner))
+    {
+      key = _get_formatted_key(self, varbindlist_scanner_get_current_key(self->varbindlist_scanner));
+      value = varbindlist_scanner_get_current_value(self->varbindlist_scanner);
+
+      log_msg_set_value_by_name(msg, key, value, -1);
+
+      msg_debug("----------------", evt_tag_str("key", key), evt_tag_str("value", log_msg_get_value_by_name(msg, key, NULL)));
+
+      msg_trace("varbindlist_scanner_scan_next",
+                evt_tag_str("key", key),
+                evt_tag_str("type", varbindlist_scanner_get_current_type(self->varbindlist_scanner)),
+                evt_tag_str("value", value));
+    }
+
+  return TRUE;
+}
+
 static gboolean
 snmptrapd_parser_process(LogParser *s, LogMessage **pmsg, const LogPathOptions *path_options,
                          const gchar *input, gsize input_len)
@@ -76,24 +208,27 @@ snmptrapd_parser_process(LogParser *s, LogMessage **pmsg, const LogPathOptions *
 
   log_msg_make_writable(pmsg, path_options);
 
-  /* header scanner */
-  /* ... */
+  /* APPEND_ZERO(input, input, input_len);? */
 
-  /* input_len? nullterminálás garantálva? */
-  const gchar *next_line = strchr(input, '\n');
-  if (!next_line)
+  if (!_parse_header(self, *pmsg, &input, &input_len) || !_parse_varbindlist(self, *pmsg, &input, &input_len))
     return FALSE;
 
-  varbindlist_scanner_input(self->varbindlist_scanner, input);
-  while (varbindlist_scanner_scan_next(self->varbindlist_scanner))
-    {
-      msg_trace("varbindlist_scanner_scan_next",
-                evt_tag_str("key", varbindlist_scanner_get_current_key(self->varbindlist_scanner)),
-                evt_tag_str("type", varbindlist_scanner_get_current_type(self->varbindlist_scanner)),
-                evt_tag_str("value", varbindlist_scanner_get_current_value(self->varbindlist_scanner)));
-    }
+  /* set default msg */
 
   return TRUE;
+}
+
+/* WORKAROUND, TODO: threadsafe scanner, formatted_key, etc. */
+static gboolean
+_process_threaded(LogParser *s, LogMessage **pmsg, const LogPathOptions *path_options,
+                  const gchar *input, gsize input_len)
+{
+  LogParser *self = (LogParser *)log_pipe_clone(&s->super);
+
+  gboolean ok = snmptrapd_parser_process(self, pmsg, path_options, input, input_len);
+
+  log_pipe_unref(&self->super);
+  return ok;
 }
 
 static LogPipe *
@@ -126,6 +261,7 @@ snmptrapd_parser_free(LogPipe *s)
 
   g_free(self->prefix);
   log_template_unref(self->message_template);
+  g_string_free(self->formatted_key, TRUE);
 
   varbindlist_scanner_free(self->varbindlist_scanner);
 
@@ -166,9 +302,11 @@ snmptrapd_parser_new(GlobalConfig *cfg)
   self->super.super.deinit = snmptrapd_parser_deinit;
   self->super.super.free_fn = snmptrapd_parser_free;
   self->super.super.clone = snmptrapd_parser_clone;
-  self->super.process = snmptrapd_parser_process;
+  self->super.process = _process_threaded;
 
   log_template_options_defaults(&self->template_options);
+
+  self->formatted_key = g_string_sized_new(32);
 
   return &self->super;
 }
