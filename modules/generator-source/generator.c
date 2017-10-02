a965c8e1197e41b8addef1658a56160a738f867d
Gabor Nagy <gabor.nagy@balabit.com>
2017-10-02 09:54:57 +0200

    generator-source: periodic events start
    Signed-off-by: Gabor Nagy <gabor.nagy@balabit.com>

--- modules/generator-source/generator.c
+++ modules/generator-source/generator.c
@@ -24,24 +24,64 @@
 #include "messages.h"
 #include "logsource.h"
 #include <string.h>
+#include <iv.h>
 
 typedef struct
 {
   LogSource super;
+  struct iv_timer generator_timer;
 } GeneratorSource;
 
 typedef struct 
 {
   LogSrcDriver super;
   GeneratorSource *generator;
+  LogSourceOptions gsource_options;
 } GeneratorSrcDriver;
+
+static void
+generator_timer_update_and_trigger(GeneratorSource *self)
+{
+  if (iv_timer_registered(&self->generator_timer))
+    {
+      msg_warning("Generator timer is already registered! Unregistering...");
+      iv_timer_unregister(&self->generator_timer);
+    }
+
+  iv_validate_now();
+  self->generator_timer.expires = iv_now;
+  self->generator_timer.expires.tv_sec += 3;
+  iv_timer_register(&self->generator_timer);
+}
+
+static void
+generator_timer_handler(void *cookie)
+{
+  GeneratorSource *self = (GeneratorSource *) cookie;
+  LogMessage *msg = log_msg_new_empty();
+  log_msg_set_value(msg, LM_V_MESSAGE, "YIPPIKAYE", -1);
+  log_source_post(&self->super, msg);
+  
+  generator_timer_update_and_trigger(self);
+}
+
+static void
+generator_timer_init(GeneratorSource *self)
+{
+  IV_TIMER_INIT(&self->generator_timer);
+  self->generator_timer.cookie = self;
+  self->generator_timer.handler = generator_timer_handler;
+}
 
 static gboolean
 generator_source_init(LogPipe *s)
 {
+  GeneratorSource *self = (GeneratorSource*) s;
+ 
   if(!log_source_init(s))
     return FALSE;
 
+  generator_timer_update_and_trigger(self);
   return TRUE;
 }
 
@@ -58,8 +98,13 @@ generator_source_new(GeneratorSrcDriver *owner)
   GlobalConfig *cfg = log_pipe_get_config(&owner->super.super.super);
 
   log_source_init_instance(&self->super, cfg);
+  log_source_set_options(&self->super, &owner->gsource_options,
+                        owner->super.super.id, "generator_source",
+                        FALSE, FALSE, owner->super.super.super.expr_node);
   self->super.super.init = generator_source_init;
   self->super.super.deinit = generator_source_deinit;
+
+  generator_timer_init(self);
   return self;
 }
 
@@ -67,10 +112,12 @@ static gboolean
 generator_sd_init(LogPipe *s)
 {
   GeneratorSrcDriver *self = (GeneratorSrcDriver *) s;
+  GlobalConfig *cfg = log_pipe_get_config(s);
 
   if (!log_src_driver_init_method(s))
     return FALSE;
 
+  log_source_options_init(&self->gsource_options, cfg, self->super.super.group);
   self->generator = generator_source_new(self);
   log_pipe_append(&self->generator->super.super, s);
   if (!log_pipe_init(&self->generator->super.super))
@@ -103,6 +150,9 @@ generator_sd_deinit(LogPipe *s)
 static void
 generator_sd_free(LogPipe *s)
 {
+  GeneratorSrcDriver *self = (GeneratorSrcDriver *) s;
+
+  log_source_options_destroy(&self->gsource_options);
   log_src_driver_free(s);
 }
 
@@ -112,6 +162,7 @@ generator_sd_new(GlobalConfig *cfg)
   GeneratorSrcDriver *self = g_new0(GeneratorSrcDriver, 1);
 
   log_src_driver_init_instance((LogSrcDriver *)&self->super, cfg);
+  log_source_options_defaults(&self->gsource_options);
   self->super.super.super.init = generator_sd_init;
   self->super.super.super.deinit = generator_sd_deinit;
   self->super.super.super.free_fn = generator_sd_free;
